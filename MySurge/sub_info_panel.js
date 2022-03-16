628ef1a5f60f575391b094fd421f27048ab844f9
chaizi <chaizia@gmail.com>
2022-03-17 01:10:15 +0800

    Update sub_info_panel.js

--- MySurge/sub_info_panel.js
+++ MySurge/sub_info_panel.js
@@ -35,7 +35,7 @@ Sub_info = script-name=Sub_info,update-interval=86400
   let used = info.download + info.upload;
   let total = info.total;
   let expire = args.expire || info.expire;
-  let content = [`已用:${bytesToSize(used)} & 剩余：${resetDayLeft}天`];
+  let content = [`已使用:${bytesToSize(used)}  &  距重置:${resetDayLeft}天`];
 
 /*
   if (resetDayLeft) {
